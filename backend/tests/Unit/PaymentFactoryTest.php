99cadcbb87bfbbe30d94ec7ff2b3a509423bfe57
Wasgton Rodrigues Junior <wasgton.it@gmail.com>
2025-02-19 19:26:34 -0300

    test: Add test for invalid payment method in PaymentFactory
    Removed outdated example tests;
    Adding a new test to validate if exception is thrown for unsupported payment methods.

--- backend/tests/Unit/PaymentFactoryTest.php
+++ backend/tests/Unit/PaymentFactoryTest.php
@@ -6,21 +6,32 @@ use App\Payments\Factories\PaymentFactory;
 use App\Payments\PaymentMethods\BoletoPayment;
 use App\Payments\PaymentMethods\CreditCardPayment;
 use App\Payments\PaymentMethods\PixPayment;
-use PHPUnit\Framework\TestCase;
+use DomainException;
+use Tests\TestCase;
 
 class PaymentFactoryTest extends TestCase
 {
     /**
-     * @throws \Exception
+     * @throws DomainException
      */
     public function test_should_get_payment_methods_from_factory() : void
     {    
         $creditCardMethod = PaymentFactory::createPaymentMethod('credit_card');
         $boletoMethod = PaymentFactory::createPaymentMethod('boleto');
         $pixMethod = PaymentFactory::createPaymentMethod('pix');
-        
+
         $this->assertInstanceOf(CreditCardPayment::class, $creditCardMethod);
         $this->assertInstanceOf(BoletoPayment::class, $boletoMethod);
         $this->assertInstanceOf(PixPayment::class, $pixMethod);
+    }
+    
+    /**
+     * @throws DomainException
+     */
+    public function test_should_get_exception_when_instantiate_invalid_method() : void
+    {    
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage("Payment method not supported");
+        PaymentFactory::createPaymentMethod('debit_card');
     }
 }
