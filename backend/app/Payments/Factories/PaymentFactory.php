b575e0c9163997420a48925baed11e248e4c077e
Wasgton Rodrigues Junior <wasgton.it@gmail.com>
2025-02-19 19:27:46 -0300

    refactor: changing exception for better understanding

--- backend/app/Payments/Factories/PaymentFactory.php
+++ backend/app/Payments/Factories/PaymentFactory.php
@@ -3,21 +3,21 @@
 namespace App\Payments\Factories;
 
 use App\Payments\PaymentMethods\BoletoPayment;
+use App\Payments\PaymentMethods\Contracts\PaymentMethod;
 use App\Payments\PaymentMethods\CreditCardPayment;
-use App\Payments\PaymentMethods\PaymentMethod;
 use App\Payments\PaymentMethods\PixPayment;
+use DomainException;
 
 class PaymentFactory
 {
-
     public static function createPaymentMethod(string $type) : PaymentMethod
     {
          return match ($type) {
              'boleto' => new BoletoPayment(),
              'credit_card' => new CreditCardPayment(),
              'pix' => new PixPayment(),
-             default => throw new \Exception("Payment method not supported")
+             default => throw new DomainException("Payment method not supported")
          };
     }
-    
+
 }
