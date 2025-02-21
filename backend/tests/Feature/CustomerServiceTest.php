e699a1cbe0298a647309dde460b68eaffd2607ff
Wasgton Rodrigues Junior <wasgton.it@gmail.com>
2025-02-20 21:20:24 -0300

    test: CustomerService
    Introducing more tests for CustomerService:
     - including customer creation;
     - database insertion validation;
     - API integration.
    Adding tests for both scenarios when data are only on database and when data are only on external API;
    Adding RefreshDatabase trait to reset database;

--- backend/tests/Feature/CustomerServiceTest.php
+++ backend/tests/Feature/CustomerServiceTest.php
@@ -2,15 +2,20 @@
 
 namespace Tests\Feature;
 
+use App\DTO\CreateCustomerDto;
 use App\Models\Customer;
 use App\Models\User;
+use App\Repositories\CustomerRepositoryEloquent;
 use App\Services\CustomerService;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CustomerServiceTest extends TestCase
 {
-    use WithFaker;
+    use WithFaker, RefreshDatabase;
 
     protected function setUpFaker(): void
     {
@@ -25,12 +30,102 @@ class CustomerServiceTest extends TestCase
         $this->actingAs($user);
         /**@var CustomerService $service*/
         $service = app(CustomerService::class);
-        $data = [
+        $create = [
             'name' => $this->faker->name,
             'document' => $this->faker->cpf(false),
             'email' => $this->faker->email
         ];
-        $service->createCustomer($data);
-        $this->assertDatabaseHas(Customer::class, $data);
+        $service->createCustomer($create);
+        $this->assertDatabaseHas(Customer::class, $create);
     }
+
+    /**
+     * @throws \HttpException
+     * @throws GuzzleException
+     */
+    public function test_should_ensure_customer_exists(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        /**@var CustomerService $service*/
+        $service = app(CustomerService::class);
+        $create = [
+            'name' => $this->faker->name,
+            'document' => $this->faker->cpf(false),
+            'email' => $this->faker->email
+        ];
+        $service->ensureCustomerExists($create);
+        $this->assertDatabaseHas(Customer::class, $create);
+    }
+
+    /**
+     * @throws \HttpException
+     * @throws GuzzleException
+     */
+    public function test_ensure_customer_should_get_from_db(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        /**@var CustomerService $service*/
+        $service = app(CustomerService::class);
+        $create = [
+            'name' => $this->faker->name,
+            'document' => $this->faker->cpf(false),
+            'email' => $this->faker->email,
+            'asaas_id' => 'cus_000000000000',
+            'user_id' => $user->id,
+        ];
+        $repository = new CustomerRepositoryEloquent();
+        $repository->create($create);
+        $customer = $service->ensureCustomerExists($create);
+        $this->assertEquals($customer->asaas_id, $create['asaas_id']);
+        $this->assertEquals($customer->name, $create['name']);
+        $this->assertEquals($customer->document, $create['document']);
+        $this->assertEquals($customer->email, $create['email']);
+        $this->assertEquals($customer->user_id, $create['user_id']);
+    }
+
+    /**
+     * @throws \HttpException
+     * @throws GuzzleException
+     */
+    public function test_should_get_customer_from_api_when_exists(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        /**@var CustomerService $service*/
+        $service = app(CustomerService::class);
+        $create = [
+            'name' => $this->faker->name,
+            'document' => $this->faker->cpf(false),
+            'email' => auth()->user()->email,
+        ];
+        $client = new Client([
+            'base_uri' => config('asaas.base_url'),
+            'allow_redirects' => false,
+            'headers' => [
+                'access_token' => config('asaas.api_key'),
+                'accept' => 'application/json',
+                'content-type'=>'application/json',
+            ],
+        ]);
+        $client->request('POST', 'customers', ['json' => $create]);
+        $this->assertDatabaseMissing('customers', $create);
+        $service->ensureCustomerExists($create);
+        $this->assertDatabaseHas('customers', $create);
+    }
+    
+//    public function test_should_get_client_from_api()
+//    {
+//        $client = new \GuzzleHttp\Client();
+//        $response = $client->request('GET', 'https://api-sandbox.asaas.com/v3/customers?cpfCnpj=75813927590', [
+//            'headers' => [
+//                'accept' => 'application/json',
+//                'access_token' => '$aact_MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OmNjODhlZjE1LTA2OGQtNDlhZi1hZmRhLTg0ZDhmOTI3NDdmZDo6JGFhY2hfNDFlYTgwM2YtNjNlNi00MGY4LWE2ODctODg3M2UxYzY0Mzdj',
+//            ],
+//        ]);
+//        $arrayResponse = json_decode((string)$response->getBody(), true);
+//        dd($arrayResponse);
+//    }
+    
 }
